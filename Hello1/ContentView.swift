25401edf0480eafc9d8e72e950e4868df1ce02f3
Peyman <peyman.osatian@gmail.com>
2024-03-04 20:56:51 +0100

    Initial Commit

---
+++ Hello1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Hello1
+//
+//  Created by Peyman Osatian on 2024-03-04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Hello1/Hello1App.swift
@@ -0,0 +1,17 @@
+//
+//  Hello1App.swift
+//  Hello1
+//
+//  Created by Peyman Osatian on 2024-03-04.
+//
+
+import SwiftUI
+
+@main
+struct Hello1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
