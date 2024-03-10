74504e6d3afa6637ed74cc523c4e94e49c377032
Peyman <peyman.osatian@gmail.com>
2024-03-10 10:16:54 +0100

    Rewording-Test

---
+++ Hello1/Screen/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  Hello1
+//
+//  Created by Peyman Osatian on 2024-03-04.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
---
+++ Hello1/Screen/SignUpView.swift
@@ -0,0 +1,18 @@
+//
+//  SignUpView.swift
+//  Hello1
+//
+//  Created by Peyman Osatian on 2024-03-10.
+//
+
+import SwiftUI
+
+struct SignUpView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SignUpView()
+}
